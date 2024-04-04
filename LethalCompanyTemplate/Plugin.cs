d238a5d00081f36e6ab15a55b16d29c2d0d16a73
coderCleric <wescamphouse@gmail.com>
2024-04-04 14:34:07 -0600

    Initial setup

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -2,13 +2,19 @@
 
 namespace LethalCompanyTemplate
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        //Plugin info
+        public const string MOD_GUID = "coderCleric.LCTweaks";
+        public const string MOD_NAME = "LCTweaks";
+        public const string MOD_VERSION = "1.0.0";
+
+
         private void Awake()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            //Say a message once we finish
+            Logger.LogInfo($"Plugin {MOD_GUID} is loaded!");
         }
     }
 }
